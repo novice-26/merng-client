878ce28b638e589c860aa8251225d00c07588219
vivek-dev-26 <viveksai0792@gmail.com>
2021-04-25 16:00:24 +0530

    changes of the server path

--- src/ApolloProvider.js
+++ src/ApolloProvider.js
@@ -7,7 +7,7 @@ import {ApolloProvider} from "@apollo/react-hooks";
 import {setContext} from 'apollo-link-context'
 
 const httpLink = createHttpLink({
-    uri:"http://localhost:5000/"
+    uri:"https://aqueous-reef-84163.herokuapp.com/"
 });
 const authLink=setContext((req,prevContext)=>{
     const token = localStorage.getItem('jwtToken');
